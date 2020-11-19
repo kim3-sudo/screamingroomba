11bb641c8777332e58b9ab2a91dbf56c70a29f78
Sejin Kim <kim3@kenyon.edu>
2020-11-19 01:11:22 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Sejin Kim'
 author = 'Sejin Kim'
 
 # The full version, including alpha/beta/rc tags
-release = '2020'
+release = 'alpha 0.2020.2.2'
 
 
 # -- General configuration ---------------------------------------------------
